1ad982189448a9726dd030cc60cb28f88dab123f
Nate Power <power2677247@gmail.com>
2022-04-13 16:16:36 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jsonplaceholder_fetch_api_example',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
